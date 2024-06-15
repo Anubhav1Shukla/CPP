e32fd51570407e429cb24e5a6553817777369e8a
Anubhav Shukla <114331440+Anubhav1Shukla@users.noreply.github.com>
2024-06-15 09:08:39 +0530

    aoftriangle

---
+++ 3.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int h,b;
+    cout<<"Enter height and breath: "<<endl;
+    cin>>h>>b;
+    cout<<"Area of rectangle is: "<<(b*h)/2<<" m^2"<<endl;
+}
+
