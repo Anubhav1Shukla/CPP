ddf075685ef0914e42e24d0df394ff4b7010d0ed
Anubhav Shukla <114331440+Anubhav1Shukla@users.noreply.github.com>
2024-06-15 08:09:40 +0530

    add two numbers

---
+++ 1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"This programm is adding two numbers "<<endl;
+    int a,b;
+    cout<<"Enter two numbers a & b"<<endl;
+    cin>>a>>b;
+    cout<<"Addition is: "<<a+b<<endl;
+}
