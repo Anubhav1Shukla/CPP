f98f6f6dd20ec6449ad50066a40af3f5eeecc8a8
Anubhav Shukla <114331440+Anubhav1Shukla@users.noreply.github.com>
2024-06-15 08:20:48 +0530

    greeting program

---
+++ 2.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string str;
+    cout<<"May I know your name "<<endl;
+    getline(cin,str);
+    // cin>>str;
+    cout<<"Welcome "<<str;
+}
