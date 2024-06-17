8a41951e445ef6f651afc2a23f8a501ecb141441
Anubhav Shukla <114331440+Anubhav1Shukla@users.noreply.github.com>
2024-06-17 23:16:32 +0530

    Array

--- 7.cpp
+++ 7.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
 
-    int A[5]={2,3,4};
+    // int A[5]={2,3,4};
 
     // for(int i=0;i<5;i++){
     //     cout<<A[i]<<endl;
@@ -10,9 +10,15 @@ int main(){
     // for(int x:A){
     //     cout<<x<<endl;  //for each loop jo array ka size hota hai uske according elements ko print karta hai
     // }
-    for(auto x:A){
-        cout<<x<<endl; //auto ka use tab karte hai jab hame koi bhi datatype pata na ho tab auto automatic datatype find kar leta hai 
-    }
+    // for(auto x:A){
+    //     cout<<x<<endl; //auto ka use tab karte hai jab hame koi bhi datatype pata na ho tab auto automatic datatype find kar leta hai 
+    // }
 
-    return 0;
+    // return 0;
+    char A[]={'A',66,'C',68};
+    for(auto x:A){
+        cout<<x<<endl;
+    }
+    //output A B C D
+
 }
