d019182a95e63d6997e6f9a93287b4565c589202
Anubhav Shukla <114331440+Anubhav1Shukla@users.noreply.github.com>
2024-09-07 22:28:17 +0530

    VectorSort

--- VectorSort.cpp
+++ VectorSort.cpp
@@ -1,10 +1,32 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int arr[]={19,15,11,10,16},n=5;
+//     sort(arr,arr+n);
+
+//     for(int x:arr){
+//         cout<<x<<" ";
+//     }
+   
+//     sort(arr,arr+n,greater<int>());
+//      cout<<endl;
+//      for (int x:arr){
+//         cout<<x<<" ";
+//      }
+// }
+
+
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int arr[]={19,15,11,10,16},n=5;
-    sort(arr,arr+n);
-
-    for(int x:arr){
+    vector<int> v={22,12,13,44,32};
+    sort(v.begin(),v.end());
+    for(int x:v){
+        cout<<x<<" ";
+    }
+    sort(v.begin(),v.end(),greater<int>());
+    cout<<endl;
+    for(int x:v){
         cout<<x<<" ";
     }
 }
