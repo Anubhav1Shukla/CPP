71b5b66df63d138ea204bf8a26680275daf75c1d
Anubhav Shukla <114331440+Anubhav1Shukla@users.noreply.github.com>
2024-09-08 08:33:40 +0530

    BubbleSortOptimized

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -1,3 +1,31 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+
+
+
+
+// void BubbleSort(int arr[],int n){
+//         for(int i=0;i<n-1;i++){
+//             for(int j=0;j<n-i-1;j++){   //jo value sort ho gyi hai use fir se check nahi karna hai isiliye n-i-1 comparision ho raha hai 
+//                 if(arr[j]>arr[j+1]){
+//                 swap(arr[j],arr[j+1]);
+//                 }
+//             }
+//         }
+// }
+// int main(){
+//     int arr[]={10,2,25,20,90},n=5;
+//     BubbleSort(arr,n);
+//     for(int i=0;i<n;i++){
+//         cout<<arr[i]<<" ";
+//     }
+// }
+
+// //-->Time Complexity-->0(n^^2)
+
+
+
+
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -5,16 +33,21 @@ using namespace std;
 
 
 void BubbleSort(int arr[],int n){
+    bool swapped;
         for(int i=0;i<n-1;i++){
+            swapped=false;//agar value pahle se sorted hai to use check nahi karna hai agar nahi hai fir aage jana hai
             for(int j=0;j<n-i-1;j++){
                 if(arr[j]>arr[j+1]){
                 swap(arr[j],arr[j+1]);
+                swapped=true;
                 }
             }
+            if(swapped==false)
+            break;
         }
 }
 int main(){
-    int arr[]={10,2,25,20,90},n=5;
+    int arr[]={10,10,10,20,10},n=5;
     BubbleSort(arr,n);
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
